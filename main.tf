d6b70cb987c8d4d8f39e3929f1bacec18b288afb
Raju <braju@Rajus-MacBook-Pro.local>
2025-04-29 13:22:55 +0530

    added terraform scripts

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = ">= 1.4.0"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
+
+
+provider "google" {
+  project = "Raju"  
+  region  = "us-west1"
+}
+
+resource "google_folder" "raju_folder" {
+  display_name = "Raju"
+  parent       = "organizations/123456789012"  # Replace with your actual Org ID
+}
+
+
+resource "google_project" "my_project" {
+  name       = "Raju_Sub_Project"
+  project_id = "Raju_Sub_Projec"  # must be globally unique
+
+  billing_account = "01CCA3-1E791F-50DFD9"  # your billing account ID
+}
